31e5896dd7e1bd110cfcb33b76affdfd925e418c
Amit Bader <capt.dreamer@gmail.com>
2014-06-07 22:02:21 -0500

    Perlin Noise Heightmap

--- Assets/WorldGen.cs
+++ Assets/WorldGen.cs
@@ -4,23 +4,26 @@ using System.Collections;
 public class WorldGen : MonoBehaviour {
 
 	public static tk2dTileMap testMap;
+	public float[,] heights;
+
 
 	// Use this for initialization
 	void Start () {
 		testMap = this.gameObject.GetComponent<tk2dTileMap>();
 		testMap.name = "Test Map";
-		testMap.width = 10;
-		testMap.height = 10;
+		testMap.width = 256;
+		testMap.height = 256;
 		testMap.Build ();
-		for (int i = 1; i<=10; i++)
-		{
-			for (int j = 1; j<=10; j++)
-			{
-
-				testMap.SetTile(i,j,0, Random.Range(0,4));
-			}
-		}
-		testMap.Build ();
+//		for (int i = 1; i<=255; i++)
+//		{
+//			for (int j = 1; j<=255; j++)
+//			{
+//
+//				testMap.SetTile(i,j,0, Random.Range(0,4));
+//			}
+//		}
+//		testMap.Build ();
+		GenerateHeights (testMap, 10f);
 	}
 	
 	// Update is called once per frame
@@ -42,5 +45,57 @@ public class WorldGen : MonoBehaviour {
 	{
 		WorldGen.testMap.SetTile(x, y, 0, Random.Range(0,4));
 		WorldGen.testMap.Build ();
+	}
+
+	public void GenerateHeights(tk2dTileMap terrain,float tileSize)
+	{
+		float max = 0;
+		float min = 100;
+		float red;
+		float green;
+		float blue;
+		float brown;
+
+		heights = new float[terrain.width, terrain.height];
+		
+		for (int i = 0; i < terrain.width; i++)
+		{
+			for (int k = 0; k < terrain.height; k++)
+			{
+				heights[i, k] = Mathf.PerlinNoise(((float)i / (float)terrain.width) * tileSize, ((float)k / (float)terrain.height) * tileSize)/10.0f;
+				if (heights[i, k] > max) { max = heights[i, k]; }
+				if (heights[i, k] < min) { min = heights[i, k]; } 
+			}
+		}
+
+		Debug.Log ("Max: " + max + " Min: " + min);
+		red = (max - min) / 4;
+		green = ((max - min) / 4) * 2;
+		blue = ((max - min) / 4) * 3;
+		brown = ((max - min) / 4) * 4;
+		Debug.Log ("Red: " + red + " Green: " + green + "Blue: " + blue + " Brown: " + brown);
+		for (int i = 0; i < terrain.width; i++)
+		{
+			for (int k = 0; k < terrain.height; k++)
+			{
+				if (heights[i, k] < red)
+				{
+					terrain.SetTile(i, k, 0, 0);
+				} else {
+					if (heights[i, k] < green)
+					{
+						terrain.SetTile(i, k, 0, 1);
+					} else {
+						if (heights[i, k] < blue)
+						{
+							terrain.SetTile(i,k, 0, 2);
+						} else {
+							terrain.SetTile(i,k,0,3);
+						}
+					}
+				}
+			}
+		}
+		terrain.Build();
 	}
 }
